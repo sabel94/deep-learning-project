2372a21d0f2befa99224dfe4987f7a7a38608e0e
sabel94 <38723433+sabel94@users.noreply.github.com>
2019-05-16 17:09:18 +0200

    Changed from mse to accuracy

--- colorize_faces/test.py
+++ colorize_faces/test.py
@@ -1,7 +1,8 @@
 __author__ = "Johan Sabel, Felix Büttner, Joel Ekelöf"
 
 
-from config import image_height, image_width, testing_path, batch_size
+from config import (image_height, image_width, testing_path,
+                    batch_size, accuracy_epsilon)
 from helpers import save_images_to_directory
 import os
 import numpy as np
@@ -10,7 +11,7 @@ from skimage import color
 
 #Make predictions for unseen grayscale images (test data set).
 def test_model(model, testing_datagen):
-    error = 0
+    accuracy = 0
     num_testing_samples = len(os.listdir(testing_path+"testing/"))
     testing_batch_size = min(num_testing_samples, 100)
     num_testing_batches = int(np.ceil(num_testing_samples / testing_batch_size))
@@ -28,9 +29,10 @@ def test_model(model, testing_datagen):
         Y_batch_true = batch[:, :, :, 1:]
         save_images_to_directory(X_batch, Y_batch_predicted,
                                  Y_batch_true, batch_index)
-        error = error + model.evaluate(X_batch / 100, Y_batch_true / 128)[0] * len(X_batch)
+        accuracy = accuracy + model.evaluate(X_batch / 100, Y_batch_true / 128)[1] * len(X_batch)
         if (batch_index == num_testing_batches - 1):
-            mse = error / num_testing_samples
-            print("mse (test data): " + str(mse))
+            accuracy = accuracy / num_testing_samples
+            print("Accuracy on test data (threshold = "+
+                  str(100*accuracy_epsilon)+"%): "+str(accuracy))
             return
         batch_index = batch_index + 1
