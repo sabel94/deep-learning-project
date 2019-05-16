2bcbab5345277ff0ce758dfc56ac3be77b639720
sabel94 <38723433+sabel94@users.noreply.github.com>
2019-05-16 16:21:26 +0200

    Update accuracy_measure.py

--- colorize_faces/accuracy_measure.py
+++ colorize_faces/accuracy_measure.py
@@ -1,10 +1,14 @@
 __author__ = "Johan Sabel, Felix Büttner, Joel Ekelöf"
 
+
 from config import image_height, image_width, accuracy_epsilon
 import tensorflow as tf
 import keras.backend as K
 
+#Compute the accuracy on a dataset.
 def accuracy(y_true, y_predicted):
+    #Each pixel where both predicted a- and b-channels are within 
+    #a threshold from the true values will affect the accuracy positively.
     threshold = accuracy_epsilon * 2 #a/b-channels ∈ [-1, 1].
     num_images = K.shape(y_true)[0]
     num_images = tf.cast(num_images, tf.float32)
