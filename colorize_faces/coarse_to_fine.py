db192de71d329a09514371a74e436a1877e4e2b1
sabel94 <38723433+sabel94@users.noreply.github.com>
2019-05-16 18:30:42 +0200

    Add files via upload

--- colorize_faces/coarse_to_fine.py
+++ colorize_faces/coarse_to_fine.py
@@ -8,16 +8,16 @@ num_values = 10
 min_log_exp = -5
 max_log_exp = -1
 
-def get_parameter_values(min_value, max_value):
+def get_parameter_values(num_values, min_log_exp, max_log_exp):
     values = []
     for i in range(num_values):
-        exp = min_value + (max_value - min_value) * np.random.rand()
+        exp = min_log_exp + (max_log_exp - min_log_exp) * np.random.rand()
         value = 10**exp
         values.append(value)
     return values
         
 if __name__ == '__main__':
-    values = get_parameter_values(min_value, max_value)
+    values = get_parameter_values(num_values, min_log_exp, max_log_exp)
     with open("parameter_values.txt", 'w') as f:
         for value in values:
             f.write(str(value)+"\n")
