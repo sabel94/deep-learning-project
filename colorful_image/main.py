75be743c98d525002d98eca2624ea038ab10cbf5
Felix Büttner <35602050+bttner@users.noreply.github.com>
2019-05-27 03:49:20 +0200

    Remove old network

--- colorful_image/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""Simple launcher."""
-
-import network.app
-import sys
-
-if __name__ == '__main__':
-    sys.exit(network.app.main())
--- colorful_image/network/__main__.py
+++
@@ -1,7 +0,0 @@
-"""Entry point. Simply execute the app."""
-
-import sys
-import network.app
-
-if __name__ == '__main__':
-    sys.exit(network.app.main())
